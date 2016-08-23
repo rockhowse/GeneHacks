1a788b5efa9bfd0bfa226f958a9192d4c9b7b03f
Nate Rock <rockhowse@gmail.com>
2016-08-23 19:44:10 -0400

    we can now extract the correct overlaps using -wo between the bam and gtf

--- cmd_line/sam_bed_tools/bedtools.sh
+++ cmd_line/sam_bed_tools/bedtools.sh
@@ -5,6 +5,8 @@
 # get a count of overlaps in filtered data set
 # http://bedtools.readthedocs.io/en/latest/content/tools/intersect.html
 #We start by running BEDtools on the alignment set restricted to the specified region (Chr3:11777000-11794000) and the GTF annotation file listed above. To allow the input to be read directly from the BAM file, we use the option ‘-abam’; in this case we will need to also specify ‘-bed’ for the BAM alignment information to be shown in BED column format in the output:
+bedtools intersect -abam ./data/athal_wu_0_A.region.bam -b ./data/athal_wu_0_A_annot.gtf -bed -wo > ./data/athal_wu_0_A.overlaps.bed
+more ./data/athal_wu_0_A.overlaps.bed | wc -l
 
 
 
