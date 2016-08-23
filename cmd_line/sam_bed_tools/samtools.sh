415df41fa42e04b725ed9bee2edb2c54586a33ea
Nate Rock <rockhowse@gmail.com>
2016-08-22 22:50:59 -0400

    get counts from extracted segment using bedtools

--- cmd_line/sam_bed_tools/samtools.sh
+++ cmd_line/sam_bed_tools/samtools.sh
@@ -11,12 +11,12 @@ samtools view ./data/athal_wu_0_A.bam | cut -f3 | grep -v '*' | wc -l
 #An alignment with an unmapped mate is marked with a ‘*’ in column 7. Note that the question asks for alignments, not reads, so we simply count the number of lines in the SAM file with a ‘*’ in column 7:
 samtools view ./data/athal_wu_0_A.bam | cut -f7 | grep -c '*'
 
-# get the count of reads that contain deletions
+# get the count of alignments that contain deletions
 #WRONG: samtools view athal_wu_0_A.bam | grep D | wc -l
 #Deletions are be marked with the letter ‘D’ in the CIGAR string for the alignment, shown in column 6:
 samtools view ./data/athal_wu_0_A.bam | cut -f6 | grep -c 'D'
 
-# get the number of reads who's mate is mapped to the same chromosome
+# get the number of alignments who's mate is mapped to the same chromosome
 #SAM docs column 7. RNEXT: Reference sequence name of the primary alignment of the NEXT read in the template. For
 #the last read, the next read is the first read in the template. If @SQ header lines are present, RNEXT (if
 #not ‘*’ or ‘=’) must be present in one of the SQ-SN tag. This field is set as ‘*’ when the information is
@@ -36,4 +36,20 @@ samtools sort ./data/athal_wu_0_A.bam ./data/athal_wu_0_A.sorted
 samtools index ./data/athal_wu_0_A.sorted.bam
 
 #extract specific range using bed file
-samtools view -L ./data/athal_wu_0_A.bed ./data/athal_wu_0_A.sorted.bam > ./data/athal_wu_0_A.extracted.bam 
+# make sure to include header
+samtools view -h -L ./data/athal_wu_0_A.bed ./data/athal_wu_0_A.sorted.bam > ./data/athal_wu_0_A.extracted.bam 
+
+#get number of alignments in extracted region
+samtools view ./data/athal_wu_0_A.extracted.bam | cut -f3 | grep -v '*' | wc -l
+
+#get number of alignments with mate unmapped in extracted region
+samtools view ./data/athal_wu_0_A.extracted.bam | cut -f7 | grep -c '*'
+
+# get number of alighnments with a deletion in extracted region
+samtools view ./data/athal_wu_0_A.extracted.bam | cut -f6 | grep -c 'D'
+
+# get number of alignments that show reads mate mapped to the same chromosome in extracted region
+samtools view ./data/athal_wu_0_A.extracted.bam | cut -f7 | grep -c '='
+
+# get the number of spliced alignments in the extracted region
+samtools view ./data/athal_wu_0_A.extracted.bam | cut -f6 | grep -c 'N'
