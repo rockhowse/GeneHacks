e308d9c812e4d38206ce8ae5a3fca1508b6b1e15
Nate Rock <rockhowse@gmail.com>
2016-08-22 21:43:27 -0400

    added samtools commands for getting the reads listed on the same chr as their mate as well as the number of spliced alignments

--- cmd_line/sam_bed_tools/samtools.sh
+++ cmd_line/sam_bed_tools/samtools.sh
@@ -1,15 +1,30 @@
 !#/bin/bash
+# these commands were used on samtools v1.2 htslib 1.2.1
+
 # get number of alignments ina a sam file
-samtools flagstat athal_wu_0_A.bam | head -n 1
+samtools flagstat ./data/athal_wu_0_A.bam | head -n 1
 # Note that, if the file was created with a tool that includes unmapped reads into the BAM file, we would need to exclude the lines representing unmapped reads, i.e. with a ‘*’ in column 3 (chrom):
-samtools view athal_wu_0_A.bam | cut -f3 | grep -v '*' | wc -l
+samtools view ./data/athal_wu_0_A.bam | cut -f3 | grep -v '*' | wc -l 
 
 # get the alignments showing the read mate's unmapped (FLAG 0x8)
 #WRONG:samtools view -f 0x8 athal_wu_0_A.bam | wc -l
 #An alignment with an unmapped mate is marked with a ‘*’ in column 7. Note that the question asks for alignments, not reads, so we simply count the number of lines in the SAM file with a ‘*’ in column 7:
-samtools view athal_wu_0_A.bam | cut -f7 | grep -c '*'
+samtools view ./data/athal_wu_0_A.bam | cut -f7 | grep -c '*'
 
 # get the count of reads that contain deletions
 #WRONG: samtools view athal_wu_0_A.bam | grep D | wc -l
 #Deletions are be marked with the letter ‘D’ in the CIGAR string for the alignment, shown in column 6:
-samtools view athal_wu_0_A.bam | cut -f6 | grep -c 'D'
+samtools view ./data/athal_wu_0_A.bam | cut -f6 | grep -c 'D'
+
+# get the number of reads who's mate is mapped to the same chromosome
+#SAM docs column 7. RNEXT: Reference sequence name of the primary alignment of the NEXT read in the template. For
+#the last read, the next read is the first read in the template. If @SQ header lines are present, RNEXT (if
+#not ‘*’ or ‘=’) must be present in one of the SQ-SN tag. This field is set as ‘*’ when the information is
+#unavailable, and set as ‘=’ if RNEXT is identical RNAME. If not ‘=’ and the next read in the template
+#has one primary mapping (see also bit 0x100 in FLAG), this field is identical to RNAME at the primary
+#line of the next read. If RNEXT is ‘*’, no assumptions can be made on PNEXT and bit 0x20
+samtools view ./data/athal_wu_0_A.bam | cut -f7 | grep -c '='
+
+# find the number of spliced alignments
+# value of 'N' in CIGAR field 6
+samtools view ./data/athal_wu_0_A.bam | cut -f6 | grep -c 'N'
