e794f026a9a6baccd8d10446fbc800545be00703
rockhowse <rockhowse@gmail.com>
2016-08-28 16:18:52 -0500

    modified vcp structure and updated script accordingly

--- cmd_line/variant_calling_pipeline/vcp.sh
+++ cmd_line/variant_calling_pipeline/vcp.sh
@@ -4,22 +4,23 @@
 # bowtie v.2.2.5
 # bcftools v.1.2
 
-#how many sequences were in the genome
+#how many sequences were in the genome (fasta file)
 cat ./data/wu_0.v7.fas | grep ">" |  wc -l
 
 #What was the name of the third sequence in the genome file?
 # Give the name only, without the “>” sign.
+# Read 3rd value
 cat ./data/wu_0.v7.fas | grep ">" 
 
 #What was the name of the last sequence in the genome file? 
 #Give the name only, without the “>” sign.
+# Read last value
 cat ./data/wu_0.v7.fas | grep ">"
 
 # create directory for wu_0
-# icd ./data/
-# mkdir wu_0
+mkdir ./data/wu_0
 
 # generate bowtie2 index of wu_0_A genome 
-# bowtie2-build wu_0.v7.fas wu_0/wu_0
+bowtie2-build ./data/wu_0.v7.fas ./data/wu_0/wu_0
 
 
