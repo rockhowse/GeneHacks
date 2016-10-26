b87a40f0f233222270bc65746e50ae83e7925e91
guest coursera <guest@centos6.host>
2016-10-26 18:38:48 -0400

    started rna_seq.sh and excluded raw data

---
+++ cmd_line/rna_seq/rna_seq.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script contains commands to do a simple RNA-seq experiment to determine genes that are differentially expressed
+#
+# required tools used:
+# - samtools    v1.2
+# - bowtie2     v2.2.2
+# - tophat      v2.0.14
+# - cufflinks   v2.2.1
+# - cuffmerege  v2.2.1
+# - cuffcompare v2.2.1
+# - cuffdiff    v2.2.1
